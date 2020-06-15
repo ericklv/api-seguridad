6b7ca4a62911543fb38073375174c823116a7efc
thanatos <elazon01@gmail.com>
2020-06-14 23:50:32 -0500

    add vigenere comments

--- src/main/java/com/caesar/demo/service/Vigenere.java
+++ src/main/java/com/caesar/demo/service/Vigenere.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Vigenere {
     private static final String alphabetDefault = "abcdefghijklmnopqrstuvwxyz";
 
+    //metodo que iguala la longitud del key el el texto
     public static String standardLength(String text, String key){
         int textLength = text.length();
         int keyLength = key.length();
@@ -23,10 +24,12 @@ public class Vigenere {
         return newKey;
     }
 
+    //verifica que el alfabeto no este vacio
     public static String checkAlphabet_ (String alphabet) {
         return StringUtils.isNotBlank(alphabet)? alphabet:alphabetDefault;
     };
 
+    //encriptacion usando un alfabeto personalizado
     public static String customEncrypt (String alphabet, String text, String key){
         List<String> alphabet_ = Arrays.asList(checkAlphabet_(alphabet).split(""));
         int length = alphabet_.size();
@@ -35,6 +38,7 @@ public class Vigenere {
         return generateCustomCipher(alphabet_,text, key_, length,1);
     };
 
+    //desencriptacion usando alfabeto personalizado
     public static String customDecrypt (String alphabet, String text, String key){
         List<String> alphabet_ = Arrays.asList(checkAlphabet_(alphabet).split(""));
         int length = alphabet_.size();
@@ -43,12 +47,17 @@ public class Vigenere {
         return generateCustomCipher(alphabet_,text, key_, length,0);
     };
 
+    //genera cifrado, la orientación le indica si encripta o descripta
+    //1 encripta
+    //0 desencripta
     private static String generateCustomCipher(List<String> alphabet, String text, String key, int length, int orientation){
         List<String> text_ = Arrays.asList(text.split(""));
         List<String> key_ = Arrays.asList(key.split(""));
         StringBuilder result = new StringBuilder();
         int keyIndex = 0;
 
+        //itera sobre cada caracter y se suman sus posiciones para obtener la posicion
+        //del nuevo caracter que sera parte del mensaje cifrado
         for (String c : text_) {
             if (alphabet.contains(c)) {
                 int pos = alphabet.indexOf(c);
